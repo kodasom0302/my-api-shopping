ad2d74afd4593b56986cfcc353ad5908239ca675
kodasom0302 <dasom589@gmail.com>
2024-05-06 18:23:54 +0900

    2024-05-06

---
+++ src/main/java/com/javaex/MyApiShoppingApplication.java
@@ -0,0 +1,13 @@
+package com.javaex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyApiShoppingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyApiShoppingApplication.class, args);
+	}
+
+}
